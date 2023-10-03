34c9ea9571748c02c8c46529128b834f8c6d130e
carlosmedina5555 <-->
2023-10-03 00:04:01 -0300

    ejercicio G

--- Js/EjecicioF-J.js
+++ Js/EjecicioF-J.js
@@ -22,3 +22,30 @@
 
 
 // G) Dado un array de 10 elementos, solicitar al usuario ingresar un valor a buscar dentro del array e indicar si dicho valor se encuentra o no dentro del array. SIN UTILIZAR EL FILTER O CUALQUIER MÉTODO DE FILTRADO
+
+
+// let array = [34, 65, 23, 63 , 84, 11 ,33, 22, 1, 5]
+// console.log(array)
+// let bandera = false
+// let valor = +prompt("ingrese en valor que quiere buscar en el array")
+// for(index = 0; index < array.length; index++) {
+//     if(array[index] === valor) {
+//         console.log(`el ${valor} ingresado no se encuentra en el array`)
+//     } else {
+//         console.log(`el ${valor} se encuentra en el array`)
+//         bandera = true
+//         break;
+//     }
+// }
+
+
+// H) Dado un array de 5 elementos dados por el usuario, ordenar sus valores de forma ascendente y descendente SIN UTILIZAR EL SORT
+
+let array = []
+let contador = 0
+while(contador < 5) {
+    let elementos = +prompt("ingrese un valor numerico")
+    array[array.length] = elementos
+    contador++
+}
+console.log(array)
