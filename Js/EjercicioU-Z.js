ded94a781b092df06826dd780b97f8335666c74b
carlosmedina5555 <carlosmedina5555@gmail.com>
2023-10-21 21:28:40 -0300

    ejercicio V resuelto

--- Js/EjercicioU-Z.js
+++ Js/EjercicioU-Z.js
@@ -14,4 +14,21 @@
 
 // V) Dibujar una pirámide y un cuadrado en la consola utilizando el símbolo *
 
-let lala = 0
+// function triangulo(filas) {
+//     for(let i = 1; i <= filas; i++) {
+//         let espacios = ' '.repeat(filas - i)
+//         let asteriscos = '*'.repeat(2 * i - 1)
+//         console.log(espacios + asteriscos)
+//     }
+// }
+// console.log("triangulo")
+// triangulo(7)
+
+// function cuadrado(lado) {
+//     for(let i = 0; i <= lado; i++) {
+//        let aste = '*'.repeat(lado)
+//        console.log(aste)
+//     }
+// }
+// console.log("cuadrado")
+// cuadrado(5)
